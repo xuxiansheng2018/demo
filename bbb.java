0ef55e911f615ee891954331a50f073c40803826
xuxiansheng2018 <3306078606@qq.com>
2025-08-14 00:10:21 +0800

    BBBTest

---
+++ bbb.java
@@ -0,0 +1,12 @@
+/**
+ * @Author xuguozhu
+ * @Create 2025/8/14 0:06
+ * Level:
+ * Description:
+ */
+public class bbb {
+    public static void main(String[] args) {
+        System.out.println("BBBB");
+        System.out.println("ZzaBBBB");
+    }
+}
