112cfbff0d8e93c9b7b5ac9a5f1dc42cde24cbc6
xuxiansheng2018 <3306078606@qq.com>
2025-08-14 00:12:28 +0800

    AAATest

---
+++ aaa.java
@@ -0,0 +1,11 @@
+/**
+ * @Author xuguozhu
+ * @Create 2025/8/14 0:06
+ * Level:
+ * Description:
+ */
+public class aaa {
+    public static void main(String[] args) {
+        System.out.println("AAA");
+    }
+}
